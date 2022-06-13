81c8b095bf75bdefbb73b519fd2ba5bb386c26d1
ansarsufiyana <ansarali106@gmail.com>
2022-06-14 04:07:26 +0500

    updated week work to repo

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+ 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper  secton__padding ' id='chef'>
+    <div className="app__wrapper_img app__wraper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="chef's word" />
+      <h1 className="headtext__cormorant">What We Belive In </h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+        </div>
+        <p className="p__opensans">Eum harum error officiis ipsa incidunt at blanditiis debitis asperiores accusamus qui, modi sapiente ratione deserunt unde porro sed consequatur. Odio, ducimus!</p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Raju raja</p>
+        <p className="p__opensans">Master Chef</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import "./Intro.css";
 
+const Intro = () => {
+  const [playvideo, setPlayVideo] = React.useState();
+  const vidRef = React.useRef();
+
+  const handelVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playvideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        typeof="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handelVideo}
+        >
+          {playvideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Intro;
